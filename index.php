c16896a3fc1bb150acafc88bcb2a62292a1d21f7
erik schievenin <erik.schievenin@gmail.com>
2022-05-03 17:31:01 +0200

    snack 4 complete

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 Snack 2
 
 Snack 3
-Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+
 Snack 4
-Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+
 Snack Bonus
-Creare un array contenente qualche alunno di un’ipotetica classe. Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.
+
 Se concludete e vi avanza tempo potete curare maggiormente la UI oppure migliorare il codice PHP con controlli sui dati :faccia_leggermente_sorridente: -->
 
 <!-- Snack 1
@@ -49,12 +49,63 @@ $name = $_GET["name"];
 $mail = $_GET["mail"];
 $age = $_GET["age"];
 
-if (strlen($name) > 3 && strpos($mail,'@')==false && strpos($mail,'.')==false) {
+if (strlen($name) > 3 && strpos($mail,'@')==true && strpos($mail,'.')==true && is_numeric($age)){
     echo('<h4>ACCESSO RIUSCITO</h4>');
 } else {
     echo('<h4>ACCESSO NON RIUSCITO</h4>');
 }
+?>
+
+<hr>
+
+<h1  style="color: red">Snack 3</h1>
+<h3 style="color: purple">Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta</h3>
+
+<?php
+
+$array = [];
+
+while (count($array) < 15 ) {
+    $random = rand(1,100);
+
+    if(!in_array($random, $array)){
+        $array[] = $random;
+    }
+}
+echo('<pre>');
+var_dump($array);
+echo('</pre>');
 
 ?>
 
+<hr>
 
+<h1  style="color: red">Snack 4</h1>
+<h3 style="color: purple">Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. 
+Ogni punto un nuovo paragrafo.</h3>
+
+<?php
+
+$paragrafo = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam magni provident nemo libero aspernatur debitis 
+ratione rem maxime excepturi nesciunt, vel, eveniet cum quaerat accusantium facere quas commodi rerum null.
+Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptas architecto quaerat nobis sint minima neque pariatur sunt tempora cumque 
+quisquam nesciunt explicabo excepturi voluptatum incidunt aspernatur porro, aliquid optio consectetur!";
+
+$succhia = explode(".", $paragrafo); 
+
+for($i=0; $i < count($succhia); $i++) {
+    echo ("<p>" . $succhia[$i] . "</p>");
+}
+
+?>
+
+<hr>
+
+<h1  style="color: red">Bonus Snack</h1>
+<h3 style="color: purple">Creare un array contenente qualche alunno di un’ipotetica classe. 
+Ogni alunno avrà Nome, Cognome e un array contenente i suoi voti scolastici. Stampare Nome, Cognome e la media dei voti di ogni alunno.</h3>
+
+<?php
+
+
+?>
